7460cd808ff01b9896ed19788eb70fa411a73191
Diego Pereira <diegopereiracalcada@gmail.com>
2023-02-17 17:54:46 -0300

    Reorganizing project folders structure + Notification and Content entities

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,15 @@
+export interface NotificationProps {
+  recipientId: string;
+  content: string;
+  category: string;
+  readAt?: Date | null;
+  createdAt: Date;
+}
+
+export class Notification {
+  private props: NotificationProps;
+
+  constructor(props: NotificationProps) {
+    this.props = props;
+  }
+}
--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { MailService } from './mail/mail.service';
-import { SmtpMailService } from './mail/smtp-mail.service';
 import { PrismaService } from './prisma.service';
 
 @Module({
