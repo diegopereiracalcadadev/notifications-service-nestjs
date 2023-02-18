b799dea13f9f503bbff45254c4f2c7b943f3a651
Diego Pereira <diegopereiracalcada@gmail.com>
2023-02-17 22:30:01 -0300

    In Memory Notifications Repository

--- src/application/entities/use-cases/send-notification.spec.ts
+++ src/application/entities/use-cases/send-notification.spec.ts
@@ -1,24 +1,19 @@
-import { Notification } from '../notification';
+import { InMemoryNotificationsRepository } from '../../../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationsRepositoryDummy = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe('Send Notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification(notificationsRepositoryDummy);
+    const inMemoryRepo = new InMemoryNotificationsRepository();
 
-    await sendNotification.execute({
+    const sendNotification = new SendNotification(inMemoryRepo);
+
+    const { notification } = await sendNotification.execute({
       content: 'This is notif.',
       category: 'social-categ',
       recipientId: 'example-recip-id',
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(inMemoryRepo.notifications).toHaveLength(1);
+    expect(inMemoryRepo.notifications[0]).toEqual(notification);
   });
 });
