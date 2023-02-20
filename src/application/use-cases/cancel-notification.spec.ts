6b8f78f4cce9a48390df0edcf75fb1831f1ca482
Diego Pereira <diegopereiracalcada@gmail.com>
2023-02-19 22:44:43 -0300

    Cancel non existent notification  test

--- src/application/use-cases/cancel-notification.spec.ts
+++ src/application/use-cases/cancel-notification.spec.ts
@@ -2,11 +2,11 @@ import { Content } from '@app/entities/content';
 import { Notification } from '@app/entities/notification';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
 
 describe('Cancel Notification', () => {
   it('should be able to cancel a notification', async () => {
     const inMemoryRepo = new InMemoryNotificationsRepository();
-
     const cancelNotification = new CancelNotification(inMemoryRepo);
 
     const mockNotification = new Notification({
@@ -22,5 +22,16 @@ describe('Cancel Notification', () => {
     });
 
     expect(inMemoryRepo.notifications[0].canceledAt).toEqual(expect.any(Date));
+  });
+
+  it('should not be able to cancel a non existent notification', async () => {
+    const inMemoryRepo = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(inMemoryRepo);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
   });
 });
